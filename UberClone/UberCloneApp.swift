aaa44c72800ba9b015c624a8c2d1ea07173762db
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-07-05 13:12:28 -0600

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Montserrat Gomez on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
