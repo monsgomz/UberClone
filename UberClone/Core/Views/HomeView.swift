90b7533e78c7165a4c88e9034e3942c00ffaad42
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-07-05 13:24:35 -0600

    Add project setup

---
+++ UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Montserrat Gomez on 05/07/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ UberClone/Core/Views/MapViewRepresentative.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewRepresentative.swift
+//  UberClone
+//
+//  Created by Montserrat Gomez on 05/07/24.
+//
+
+import Foundation
